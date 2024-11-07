e6ba7db87c1dcf9f5f64695614585143555cc3b2
Phil Jin Sang <phil@Philui-MacBookPro.local>
2024-11-07 18:23:52 +0900

    updated eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
       rules: {
         "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-interface": "off",
         // Add other rules to disable only in production
       },
     },
