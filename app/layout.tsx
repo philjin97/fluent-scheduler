516b49caa5d566998477e55f5c0d1de198714bfb
Phil Jin Sang <phil@Philui-MacBookPro.local>
2024-11-07 15:20:51 +0900

    updated errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,6 @@ import Navigation from "@/components/navigation";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
-const inter = Noto_Sans_KR({
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Fluent",
   description: "학원 서비스 폼",
